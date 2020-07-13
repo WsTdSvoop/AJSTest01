c5980a5282f4ebd0a2943ef98654384cc987185b
Triton <triton@Administrators-iMac.local>
2020-07-13 14:13:22 +0200

    step 3

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,53 +1,71 @@
-/* globals Chart:false, feather:false */
+/**==========================================================================
+    Get information from https://blockchain.info/ticker
+   ==========================================================================**/
+let urlInfo = "https://blockchain.info/ticker";
+let getInfo = new XMLHttpRequest();
+getInfo.open('GET', urlInfo);
+getInfo.responseType = 'text';
 
-(function () {
-  'use strict'
 
-  feather.replace()
 
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-}())
+getInfo.onload = function() {
+	var datainfo = JSON.parse(getInfo.response);
+	for( let bitcoininfo in datainfo ) {
+		if(bitcoininfo == "EUR") {
+			value = datainfo[bitcoininfo];
+			for(let i in value) {
+				if(i == "sell") {
+					sellEUR.textContent = "Verkaufspreis: " + value[i] + " €";
+        		}
+        		if(i == "buy") {
+          			buyEUR.textContent = "Einkaufspreis: " + value[i] + " €";
+        		}
+      		}
+    	}
+		if(bitcoininfo == "USD") {
+			value = datainfo[bitcoininfo];
+			for(let i in value) {
+				if(i == "sell") {
+					sellUSD.textContent = "Verkaufspreis: " + value[i] + " $";
+        		}
+        		if(i == "buy") {
+          			buyUSD.textContent = "Einkaufspreis: " + value[i] + " $";
+        		}
+      		}
+    	}
+		if(bitcoininfo == "AUD") {
+			value = datainfo[bitcoininfo];
+			for(let i in value) {
+				if(i == "sell") {
+					sellAUD.textContent = "Verkaufspreis: " + value[i] + " A$";
+        		}
+        		if(i == "buy") {
+          			buyAUD.textContent = "Einkaufspreis: " + value[i] + " A$";
+        		}
+      		}
+    	}
+		if(bitcoininfo == "NZD") {
+			value = datainfo[bitcoininfo];
+			for(let i in value) {
+				if(i == "sell") {
+					sellNZD.textContent = "Verkaufspreis: " + value[i] + " NZ$";
+        		}
+        		if(i == "buy") {
+          			buyNZD.textContent = "Einkaufspreis: " + value[i] + " NZ$";
+        		}
+      		}
+    	}
+		if(bitcoininfo == "GBP") {
+			value = datainfo[bitcoininfo];
+			for(let i in value) {
+				if(i == "sell") {
+					sellGBP.textContent = "Verkaufspreis: " + value[i] + " £";
+        		}
+        		if(i == "buy") {
+          			buyGBP.textContent = "Einkaufspreis: " + value[i] + " £";
+        		}
+      		}
+    	}
+	}
+};
+getInfo.send();
